a92cae8625a5f75bb7f951fd97d39327c29d6d46
mudiajohn <mudiajohn@yahoo.com>
2023-02-26 00:34:32 +0000

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h> 
+/**
+ * main - print a line of code using put
+ * 
+ * Return: Always 0 (Success) 
+ * 
+ */
+
+int main (void)
+{
+	 puts("\"Programming is like building a multilingual puzzle")
+		 return (0);
+}
