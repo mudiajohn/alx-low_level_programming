e85ddbbea0dcedf065b43f2cbe2906ddc627c1d5
Mudiajohn <mudiajohn@yahoo.com>
2023-02-26 02:56:38 +0100

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
-#include <stdio.h> 
+#include <stdio.h>
 /**
- * main-print a line of code using put
- * Return: Always 0 
- */
+* main-print a line of code using put
+* Return: Always 0 
+*/
 int main(void)
 {
-	 puts("\"Programming is like building a multilingual puzzle");
-         return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
