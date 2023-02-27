87882456c7f604054f8fef4a46417d0df0a03e3f
Mudiajohn <mudiajohn@yahoo.com>
2023-02-27 23:12:07 +0100

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,8 @@
 #include "main.h"
-
 /** 
 * main - Entry point
 * Return: Always 0
 */
-
 int main(void)
 {
 _putchar('o');
@@ -16,5 +14,4 @@ _putchar('a');
 _putchar('r');
 _putchar('n');
 return (0);
-
 }
