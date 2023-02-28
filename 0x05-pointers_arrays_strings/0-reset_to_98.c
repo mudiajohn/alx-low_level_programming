e80823558527c279939461f183708faec87abcc3
Mudiajohn <mudiajohn@yahoo.com>
2023-02-28 20:27:55 +0100

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* reset_to_98 - To Take a pointer to an int and update
+* The value it point to 
+*
+* @n: An integer input pointer
+*
+* Return: nothing
+*/
+void reset_to_98(int *n)
+{
+      *n = 98;
+}
