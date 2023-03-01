7d67835c29cd96e1265583039a6c6ded636ac218
Mudiajohn <mudiajohn@yahoo.com>
2023-03-01 01:24:05 +0100

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -3,13 +3,14 @@
 * puts2 - function should print only one character out of two
 * starting with the first one
 * @str: input
-* Return: print
+* Return: print 0
 */
 void puts2(char *str)
 {
 int longi = 0;
 int t = 0;
 char *y = str;
+int o;
 while (*y != '\0')
 {
 y++;
