8ff20b6f9de58f21b811b106aca0d293a815359a
Mudiajohn <mudiajohn@yahoo.com>
2023-03-01 21:28:02 +0100

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -23,7 +23,7 @@ printf("last digit of %d is %d and is 0\n", n, ld);
 }
 else
 {
-printf("last digit of %d is %d and less than 6 and not 0\n", n, ld);
+printf("last digit of %d is %d and is less than 6 and not 0\n", n, ld);
 }
 return (0);
 }
