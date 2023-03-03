8dc0d9b7e41fc31dcd0e45ba6f9342d075d1f667
Mudiajohn <mudiajohn@yahoo.com>
2023-03-03 03:53:57 +0100

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,7 +15,7 @@ printf("Size of a int: %lu byte(s)\n", (unsigned long)sizeof(b));
 printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
 printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
-printf(" \n ");
+printf("0\n");
 
 	return (0);
 }
