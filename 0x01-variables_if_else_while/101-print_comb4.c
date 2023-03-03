58e22eb493acb7fe16dbdc8cfdd2210f824986c2
Mudiajohn <mudiajohn@yahoo.com>
2023-03-03 01:09:27 +0100

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -3,21 +3,24 @@
 #include <stdio.h>
 /**
 *main - Entry point
-*Description: 'Combination of brain'
+*Description: 'Success Combination'
 * Return: always 0
 */
 int main(void)
 {
-int d, p;
-for (d = '0'; d < '9'; d ++)
+int d, p, q;
+for (d = '0'; d < '9'; d++)
 {
 for (p = d + 1; p <= '9'; p++)
 {
-if (p != d)
+for (q = p +1; q <= '9'; q++)
+{
+if ((p != d) !=q)
 {
 putchar(d);
 putchar(p);
-if (d == '8' && p == '9')
+putchar(q);
+if (d == '7' && p == '8')
 continue;
 putchar(',');
 putchar(' ');
