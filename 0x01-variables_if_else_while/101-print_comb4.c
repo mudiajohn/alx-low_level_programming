06518308e30f37175a4fea39d9045bd8de4b5223
Mudiajohn <mudiajohn@yahoo.com>
2023-03-03 01:14:03 +0100

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -13,9 +13,9 @@ for (d = '0'; d < '9'; d++)
 {
 for (p = d + 1; p <= '9'; p++)
 {
-for (q = p +1; q <= '9'; q++)
+for (q = p + 1; q <= '9'; q++)
 {
-if ((p != d) !=q)
+if ((p != d) != q)
 {
 putchar(d);
 putchar(p);
