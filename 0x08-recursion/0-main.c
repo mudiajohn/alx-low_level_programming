1d8e122965fa48571e27366bfc490f55f0cabe96
Mudiajohn <mudiajohn@yahoo.com>
2023-03-08 16:38:40 +0100

    0-puts_recursion.c

---
+++ 0x08-recursion/0-main.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    _puts_recursion("Puts with recursion");
+    return (0);
+}
+
+vi 1-main.c
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    _print_rev_recursion("\nColton Walker");
+    return (0);
+}
