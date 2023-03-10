483d80306f83eb32882c2c0819f4924a2b154391
Mudiajohn <mudiajohn@yahoo.com>
2023-03-10 18:40:36 +0100

    create_static_lib.sh

--- 0x08-recursion/0-main.c
+++ 0x08-recursion/0-main.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * main - check the code
  *
@@ -8,19 +7,5 @@
 int main(void)
 {
     _puts_recursion("Puts with recursion");
-    return (0);
-}
-
-vi 1-main.c
-#include "main.h"
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    _print_rev_recursion("\nColton Walker");
     return (0);
 }
