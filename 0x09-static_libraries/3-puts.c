bfa14268d95c3752ddb38abc94d9fe89b1a7e424
Mudiajohn <mudiajohn@yahoo.com>
2023-03-11 04:32:30 +0100

    libmy.a, main.h

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,14 +1,14 @@
 #include "main.h"
-#include<stdio.h>
-void _puts(char *str)
-
+#include <stdio.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+* main - Entry point
+* Return: Always 0 (Success)
+*/
+void _puts(char *str)
+{
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
 }
