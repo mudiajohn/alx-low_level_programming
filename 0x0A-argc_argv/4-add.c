48db37872a74df03049761c6b220211ab82a225d
Mudiajohn <mudiajohn@yahoo.com>
2023-03-11 05:08:48 +0100

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -39,7 +39,29 @@ while (count < argc) /*Goes through the whole array*/
 {
 if (check_num(argv[count]))
 {
+int count;
+int str_to_int;
+int sum = 0;
+count = 1;
+while (count < argc) /*Goes through the whole array*/
+{
+if (check_num(argv[count]))
+{
 str_to_int = atoi(argv[count]); /*ATOI --> convert string to int*/
+sum += str_to_int;
+}
+/*Condition if one of the number contains symbols that are not digits*/
+else
+{
+printf("Error\n");
+return (1);
+}
+count++;
+}
+printf("%d\n", sum); /*print sum*/
+return (0);
+}
+4-str_to_int = atoi(argv[count]); /*ATOI --> convert string to int*/
 sum += str_to_int;
 }
 /*Condition if one of the number contains symbols that are not digits*/
