b5d1e526ba4a043c6a91a259e60e2a36c1e4b988
Mudiajohn <mudiajohn@yahoo.com>
2023-03-22 17:20:58 +0100

    3-op_functions.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,5 +1,6 @@
 #include "3-calc.h"
 #include <stdlib.h>
+#include <string.h>
 /**
 * get_op_func - Selects the correct function to perform the operation asked by the user.
 * @s: The operator passed as argument.
@@ -8,15 +9,19 @@
 int (*get_op_func(char *s))(int, int)
 {
 op_t ops[] = {
-{"+", op_add},
-{"-", op_sub},
-{"*", op_mul},
-{"/", op_div},
-{"%", op_mod},
-{NULL, NULL},
+{ "+", op_add },
+{ "-", op_sub },
+{ "*", op_mul },
+{ "/", op_div },
+{ "%", op_mod },
+{ NULL, NULL },
 };
 int i = 0;
-while (ops[i].op != NULL && *(ops[i].op) != *s)
-i++;
+while (i < 5)
+{
+if (strcmp(s, ops[i].op) == 0)
 return (ops[i].f);
+i++;
+}
+return (0);
 }
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,7 +1,6 @@
 #include "3-calc.h"
 #include <stdlib.h>
 #include <stdio.h>
-
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
@@ -45,6 +44,11 @@ return (a * b);
 */
 int op_div(int a, int b)
 {
+if (b == 0)
+{
+printf("Error\n");
+exit(100);
+}
 return (a / b);
 }
 /**
@@ -55,5 +59,10 @@ return (a / b);
 */
 int op_mod(int a, int b)
 {
- return (a % b);
+if (b == 0)
+{
+printf("Error\n");
+exit(100);
+} 
+return (a % b);
 }
